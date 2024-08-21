8d8d033c5b764a0fb89f6db371848e92f18ae992
DandyGeorge <dandyfriday31@gmail.com>
2024-08-21 03:38:00 +0100

    Header responsiveness fix

--- src/pages/Header.tsx
+++ src/pages/Header.tsx
@@ -3,34 +3,33 @@ import { MdOutlineMenuOpen } from "react-icons/md";
 import BackArrow from '../components/BackArrow';
 
 const Header = () => { 
-  const [isMenuOpen, setIsMenuOpen] = useState(false); 
+  const [isMenuOpen, setIsMenuOpen] = useState(true); 
     return (
-        <div className="w-full">
-                           
+        <div className="w-full">                           
           <header className="fixed top-0 left-0 right-0 flex justify-between w-full px-4 pt-3 bg-transparent">  
-           <div className="flex items-center gap-6">
+           <div className="flex items-center gap-4 md:gap-6">
             <MdOutlineMenuOpen 
-            color='#1f4b43' 
-            className="text-white cursor-pointer md:hidden"
+            color='#1F3B4D' 
+            className="text-white  cursor-pointer md:hidden"
             size={32}             
             onClick={() => setIsMenuOpen(!isMenuOpen)}
             /> 
             <BackArrow /> 
 
             <h1 className="text-white ">DANDY</h1>  
-            </div>                      
+            </div>                   
            
-            <nav className='flex justify-center items-center gap-8 text-white '>
-             <a href="#" className="md:inline-block hidden">Home</a>
+            <nav className={`flex justify-center items-center gap-8 text-white ${isMenuOpen ? 'block' : 'hidden'} md:flex`}>
+             <a href="#" className="md:inline-block hidden" >Home</a>
              <a href="#" className="md:inline-block hidden">Solution</a>
              <a href="#" className="md:inline-block hidden">Pricing</a>
               <a href="#" className="md:inline-block hidden">Enterprise</a>
             </nav>
-            <div className="w-auto space-x-6 mr-3">
-              <button className="px-5 py-1 bg-gray-900 text-white 
-                rounded-2xl hover:bg-white/45">Login</button>
-              <button className="px-4 py-1 bg-red-500
-             text-white rounded-2xl hover:bg-white/45">Logout</button>
+            <div className="w-auto space-x-4 mr-12 py-2">
+              <button className="px-5 py-2 bg-gray-900 text-white 
+                rounded-md hover:bg-white/45">Login</button>
+              <button className="px-4 py-2 bg-red-500
+             text-white rounded-md hover:bg-white/45">Logout</button>
             </div>                            
           </header> 
                    
